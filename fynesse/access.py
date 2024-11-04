ba1ad94de262671faa98a876c7f273a7519e4981
Katy <kt526@cam.ac.uk>
2024-11-04 18:03:09 +0000

    Test commit

--- fynesse/access.py
+++ fynesse/access.py
@@ -11,6 +11,19 @@ import sqlite"""
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
 
+# Practical 1 price data
+def download_price_paid_data(year_from, year_to):
+        base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
+        file_name = "/pp-<year>-part<part>.csv"
+
+        for year in range(year_from, (year_to+1)):
+            print(f"Downloading data for year: {year}")
+            for part in range(1,3):
+                url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
+                response = requests.get(url)
+                if repsonse.status_code == 200:
+                    with("." + file_name.replace("<year>", str(year)).replace("<part>", "wb")) as file:
+                        file.write(response.content)
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
     raise NotImplementedError
