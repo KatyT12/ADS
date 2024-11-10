1266a6a5bc4f6600f466f0685eed7e34db489f01
Katy <kt526@cam.ac.uk>
2024-11-10 21:52:14 +0000

    Update bounding box function

--- fynesse/util.py
+++ fynesse/util.py
@@ -1,7 +1,9 @@
 def get_bounding_box(latitude: float, longitude: float, distance_km: float = 1.0):
-  distance = distance_km / 111.2
-  north = latitude + distance/2
-  south = latitude - distance/2
-  west = longitude - distance/2
-  east = longitude + distance/2
+  distance_lat = distance_km / 111.2
+  distance_long = distance_km / (111.32 * math.cos(math.radians(latitude)))
+
+  north = latitude + (distance_lat/2)
+  south = latitude - (distance_lat/2)
+  west = longitude - (distance_long/2)
+  east = longitude + (distance_long/2)
   return (north, south, east, west)
