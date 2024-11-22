ee8c7c5c84fd6c8d363ece698a882dd1ca3bfed3
Katy <kt526@cam.ac.uk>
2024-11-22 17:59:49 +0000

    Change credentials

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ VERSION = "0.1.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "pandas", "numpy", "jupyter", "matplotlib", "osmnx", "seaborn", "pymysql", "statsmodels", "ipywidgets", "yaml"
+    "pandas", "numpy", "jupyter", "matplotlib", "osmnx", "seaborn", "pymysql", "statsmodels", "ipywidgets"
 ]
 
 # What packages are optional?
