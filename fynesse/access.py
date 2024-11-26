0cb0d2076ae46723c7260e62ea362181b097a49b
Katy <kt526@cam.ac.uk>
2024-11-26 17:21:08 +0000

    Flexible distance count

--- fynesse/access.py
+++ fynesse/access.py
@@ -469,11 +469,15 @@ def create_count_table(conn, distance, tags_excluded):
    conn.cursor().execute(query)
    conn.commit()
   
-def insert_into_count_table(conn, distance):
+def insert_into_count_table(conn, distance, tags_filter):
   lat_dist, lon_dist = latlong_to_km(52.5152422, -1.1482686, distance/2, distance/2)
+  
+  tags_list = [ f'\'{s}\'' for s in flatten_tags(tags_filter)]
+  tag_string = '(' + ', '.join(tags_list) + ')'
+  
   query = f"""
       insert into code_count_table (geography_code, tag, count(*), distance)
-      select geography_code, tag, count(*), {distance} as distance from nssec_data as a join building_tag_data as b on (b.latitude between a.latitude - {lat_dist} and a.latitude + {lat_dist} and b.longitude between a.longitude - {lon_dist} and a.longitude + {lon_dist})  group by geography_code, tag;
+      select geography_code, tag, count(*), {distance} as distance from nssec_data as a join (select * from building_tag_data where tag in {tag_string}) as b on (b.latitude between a.latitude - {lat_dist} and a.latitude + {lat_dist} and b.longitude between a.longitude - {lon_dist} and a.longitude + {lon_dist})  group by geography_code, tag;
   """
   print(query)
   conn.cursor().execute(query)
