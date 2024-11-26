40887fb5d1959adf71dad3eb4cbbe28741319069
Katy <kt526@cam.ac.uk>
2024-11-26 17:21:25 +0000

    Add util

--- fynesse/util.py
+++ fynesse/util.py
@@ -1,9 +1,8 @@
-def get_bounding_box(latitude: float, longitude: float, distance_km: float = 1.0):
-  distance_lat = distance_km / 111.2
-  distance_long = distance_km / (111.32 * math.cos(math.radians(latitude)))
-
-  north = latitude + (distance_lat/2)
-  south = latitude - (distance_lat/2)
-  west = longitude - (distance_long/2)
-  east = longitude + (distance_long/2)
-  return (north, south, east, west)
+def flatten_tags(tags):
+  tags_list = []
+  for t in tags:
+    if isinstance(tags[t], list):
+      tags_list += tags[t]
+    else:
+      tags_list.append(t)
+  return tags_list
