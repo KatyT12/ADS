84b1e6416c237ac303a81cbe823305a9504f34c9
Katy <kt526@cam.ac.uk>
2024-11-28 12:58:12 +0000

    Address

--- fynesse/address.py
+++ fynesse/address.py
@@ -18,13 +18,26 @@ import scipy.stats"""
 from .assess import *
 from .access import *
 
-def query_training_for_output_area(conn, geography_code, table='household_vehicle_data', pred_table='nssec_data', pred_var='L15', census_tags=['no_vehicle_ratio', 'one_vehicle_ratio']):
+# To be used for both training and prediction of student proportions
+def get_student_design_matrix(training):
+  training['university_logged'] = np.log(training['university'].to_numpy()+1)
+  training['dorm_logged'] = np.log(training['dormitory'].to_numpy()+1)
+  training['apartments_logged'] = np.log(training['apartments'].to_numpy()+1)
+
+  features = [training['university_logged'], training['no_vehicle_ratio'], training['apartments']]
+  actual = training['training']
+  X = np.column_stack(features)
+
+  return sm.add_constant(X, has_constant='add'), actual
+
+def query_training_for_output_area(conn, geography_codes, table='household_vehicle_data', pred_table='nssec_data', pred_var='L15', census_tags=['no_vehicle_ratio', 'one_vehicle_ratio']):
+  codes = ', '.join([ '\'' + s + '\'' for s in geography_codes])
   query = f'''
     select *
     from code_count_table as cc
     join {table} as hv on hv.geography_code = cc.geography_code 
     join {pred_table} as ns on ns.geography_code = cc.geography_code 
-    WHERE cc.geography_code = '{geography_code}';
+    WHERE cc.geography_code in ({codes});
   '''
   df = query_to_dataframe(conn, query)
   ret = extract_training_data(df, census_tags, pred_var)
@@ -34,6 +47,6 @@ def find_nearest_output_areas(connection, latitude, longitude, number=1):
   query = f'''select geography_code, sqrt(pow(latitude - {latitude}, 2) + pow(longitude - {longitude}, 2)) as distance
           from nssec_data
           order by distance asc
-          limit 1;'''
+          limit {number};'''
   
   return query_to_dataframe(connection, query)
