54118eb635ac0717ea8ea958fb8e8f3dd09ec141
Katy <kt526@cam.ac.uk>
2024-12-06 16:43:54 +0000

    Add K folding mechanism

--- fynesse/address.py
+++ fynesse/address.py
@@ -17,6 +17,7 @@ import scipy.stats"""
 """Address a particular question that arises from the data"""
 from .assess import *
 from .access import *
+from sklearn.metrics import r2_score
 import statsmodels.api as sm
 
 # To be used for both training and prediction of student proportions
@@ -279,3 +280,74 @@ def augment_avg_price_oa(connection, df, types, codes=[], random_number=None, la
   merged = df.merge(prices, left_on=label, right_on='oa21', how='left')
   med = merged['avg(price)'].median()
   return merged.fillna(med)
+
+
+
+def split_data(k, n):
+  ind = np.random.choice(range(n), size=(n,), replace=False)
+  k_sets = np.array_split(ind, k)
+
+  return k_sets
+
+# Retrieve the training data from the splitted indices
+def get_train(splitted, d, i):
+
+  left = [d.iloc[splitted[j]] for j in range(i)]
+  right = [d.iloc[splitted[j]] for j in range(i+1,len(splitted))]
+  if i == 0:
+      return pd.concat(right)
+  elif i == len(splitted)-1:
+      return pd.concat(left)
+  else:
+    return pd.concat([pd.concat(left), pd.concat(right)])
+
+  total = np.concatenate(left, right)
+  return total
+
+def rmse_calc(predicted, actual):
+  return np.sqrt(((predicted - actual)**2).mean())
+
+
+# Training data - augmented, with resposne
+# Subset of training data
+def run_on_lad_subset(connection, training, subset_training, subset_test, response_col, design_func, regularized = False, alpha = 0.1, weight = 1.0):
+  # Train
+  
+  if regularized:
+    model = fynesse.address.fit_model_OLS(connection, subset_training, subset_training, response_col, design_func, augmented=subset_training, ridge=alpha, reg_weight=weight)
+  else:
+    model = fynesse.address.fit_model_OLS(connection, subset_training, subset_training, response_col, design_func, augmented=subset_training)
+  
+  X = design_func(subset_test, training)
+  prediction = model.predict(X)
+  actual = subset_test[response_col]
+
+  # Extract data
+  c = pd.DataFrame({
+      "actual": actual,
+      "predicted": prediction
+      })
+
+  corr = c.corr()['actual'].iloc[1]
+  r2 = r2_score(actual, prediction)
+  rmse = rmse_calc(prediction, actual)
+
+  #print(corr, rmse, r2)
+  return [corr, rmse, r2]
+
+
+def get_k_folded_results(k, training, response, design_func, regularized = False, alpha = 0.01, weight = 1.0):
+  n = len(training.index)
+  splitted = split_data(k, n)
+
+  ret = []
+  for i in range(len(splitted)):
+    training_data = get_train(splitted, training, i)
+    test_data = training.iloc[splitted[i]]
+
+    #print(age_df.shape, a.shape, b.shape)
+    ret.append(run_on_lad_subset(connection, training, training_data, test_data, response, design_func, regularized=regularized, alpha=alpha, weight=weight))
+
+  dat = pd.DataFrame(ret)
+  dat.columns = ['corr', 'rmse', 'r2']
+  return dat
